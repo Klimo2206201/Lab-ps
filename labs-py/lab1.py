dbf73e92d41b63a1b9a833977acdd8caa4ad91c9
Klimo2206201 <55152665+Klimo2206201@users.noreply.github.com>
2019-12-09 23:50:53 +0300

    Update lab1.py

--- labs-py/lab1.py
+++ labs-py/lab1.py
@@ -1,16 +1,21 @@
 from math import *
-
-a = float(input())
-
+print(" Enter x: ")
 x = float(input())
-
-G = 5*((-9)*a*a + 11*a*x + 14*x*x)/(3*a*a + 49*a*x + 24*x*x)
-
-F = tan(18*a*a + 29*a*x + 10*x*x )
-
-Y = acos((-7)*a*a - 10*a*x + 8*x*x + 1)
-
+print(" Enter a: ")
+a = float(input())
+G =5*((-9)*a*a +  11*a*x + 14*x*x)/(15*a*a + 49*a*x + 24*x*x );
 print(G)
 
+print(" Enter x: ")
+x = float(input())
+print(" Enter a: ")
+a = float(input())
+F =tan( 18*a*a + 29*a*x + 10*x*x );
 print(F)
 
+print(" Enter x: ")
+x = float(input())
+print(" Enter a: ")
+a = float(input())
+Y = acos(-7*a*a - 10*a*x + 8*x*x + 1);
+print(Y)
